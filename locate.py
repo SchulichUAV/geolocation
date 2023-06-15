945aa4ac56423e1048f3b7194b94c0be39d32384
Charlie-Zheng <Charlie-Zheng@users.noreply.github.com>
2023-06-14 20:00:08 -0600

    Updated locate.py to account for roll and pitch

--- locate.py
+++ locate.py
@@ -11,21 +11,24 @@ from geographiclib.geodesic import Geodesic
 # img_height_px - height of the image in pixels
 # obj_x_px - x coordinate of the target object in pixels where (0,0) is the center of the image
 # obj_y_px - y coordinate of the target object in pixels
+# roll - the roll of the uav, where positive roll is a left bank
+# pitch - the pitch of the uav, where positive pitch is nose up
 # Returns:
 # (obj_longitude, obj_latitude) - the estimated longitude and latitude of the object
 
-def locate(uav_latitude, uav_longitude, uav_altitude, bearing, cam_fov, img_width_px, img_height_px, obj_x_px, obj_y_px):
+def locate(uav_latitude, uav_longitude, uav_altitude, bearing, cam_fov, img_width_px, img_height_px, obj_x_px, obj_y_px, roll=0, pitch=0):
     cam_fov_rad = math.radians(cam_fov)
     diagonal_dist = math.tan(cam_fov_rad) * uav_altitude
     img_width = math.sqrt(diagonal_dist**2 / (1 + img_height_px / img_width_px)) # w^2 + (h/w)*w^2 = diag^2
     img_height = img_height_px / img_width_px * img_width
     length_per_px = img_width / img_width_px
-    obj_x = obj_x_px * length_per_px
-    obj_y = obj_y_px * length_per_px
+    obj_x = obj_x_px * length_per_px + uav_altitude * math.tan(roll/180*math.pi)
+    obj_y = obj_y_px * length_per_px + uav_altitude * math.tan(pitch/180*math.pi)
     dist = math.sqrt(obj_x**2 + obj_y**2)
 
     angle = math.atan2(obj_y_px, obj_x_px) # Polar angle of the object point P
-    true_bearing = (bearing + 90 - math.degrees(angle)) % 360
+    true_bearing = (bearing + 90 - math.degrees(angle) + 360) % 360
+    print(f"{true_bearing=}")
     geod = Geodesic.WGS84
     azil = true_bearing # Angle from North CW of the object point P
     g = geod.Direct(uav_latitude, uav_longitude, azil, dist)
